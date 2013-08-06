a23067ad5aaf757f6ceb0a83dd6370385eabd9fe
Troy Ready <troyr1@a011111.biola.edu>
2013-08-06 13:13:22 -0700

    Adding multi-collection support, courtesy of David Ross Smith

--- mtgassistant_export.py
+++ mtgassistant_export.py
@@ -5,49 +5,81 @@ def main():
   import argparse
   parser = argparse.ArgumentParser(description='Takes Magic Assistant collections & turn them into DeckBox CSVs.')
   parser.add_argument('--inputfile', dest='collection', default='', help='Set a specific collection to be converted. (default=~/Dropbox/MagicAssistantWorkspace/magiccards/Collections/main.xml')
-  parser.add_argument('--inventoryoutputfile', dest='inventoryoutputfile', default='', help='Full path to the desired inventory output file. Requires --decklistoutputfile if used. (default=magicdeckboxinventory.csv in the output directory option)')
-  parser.add_argument('--decklistoutputfile', dest='decklistoutputfile', default='', help='Full path to the desired decklist output file. Requires --inventoryoutputfile if used. (default=magiccollection.dec in the output directory option)')
-  parser.add_argument('--outputdirectory', dest='outputdirectory', default='', help='Directory in which to place the generated inventory & decklist files. (default=~/Dropbox, ~/Ubuntu One, or the current working directory)')
-  #parser.add_argument("--singlecollectiononly", help="Don't recursively look for collections to convert", action="store_true")
+  parser.add_argument('--inputdir', default='', help='Set a directory to search for files which comprise your collection. (default=~/Dropbox/MagicAssistantWorkspace/)')
+  parser.add_argument('--inventoryoutputfile', default='', help='Full path to the desired inventory output file. Requires --decklistoutputfile if used. (default=magicdeckboxinventory.csv in the output directory option)')
+  parser.add_argument('--decklistoutputfile', default='', help='Full path to the desired decklist output file. Requires --inventoryoutputfile if used. (default=magiccollection.dec in the output directory option)')
+  parser.add_argument('--outputdir', default='', help='Directory in which to place the generated inventory & decklist files. (default=~/Dropbox, ~/Ubuntu One, or the current working directory)')
+  parser.add_argument("--singlecollectiononly", help="Don't recursively look for collections to convert", action="store_true")
   args = parser.parse_args()
   
   import os
   home = os.path.expanduser("~")
 
-  if args.inventoryoutputfile != '' or args.inventoryoutputfile != '':
-    if (args.inventoryoutputfile != '' and args.inventoryoutputfile) or (args.inventoryoutputfile == '' and args.inventoryoutputfile != ''):
-      print('--inventoryoutputfile & --decklistoutputfile must both be specified or left to default')
-      from sys import exit
-      exit()
-    else:
-      inventoryOutputFileStr = args.inventoryoutputfile
-      decklistOutputFileStr = args.decklistoutputfile
-  elif not args.outputdirectory == '':
-    inventoryOutputFileStr = args.outputdirectory + '/magicdeckboxinventory.csv'
-    decklistOutputFileStr = args.outputdirectory + '/magiccollection.dec'
+  if not args.outputdir == '':
+    outputdirectoryprefix = args.outputdir
+    inventoryOutputFileStr = args.outputdir + '/magicdeckboxinventory.csv'
+    decklistOutputFileStr = args.outputdir + '/magiccollection.dec'
   else:
     if os.path.exists(home + '/Dropbox'):
-      outputdirectoryprefix = home + '/Dropbox'
+      outputdirectoryprefix = home + '/Dropbox/DeckboxExports'
     elif os.path.exists(home + '/Ubuntu One'):
-      outputdirectoryprefix = home + '/Ubuntu One'
+      outputdirectoryprefix = home + '/Ubuntu One/DeckboxExports'
     else:
       outputdirectoryprefix = os.getcwd()
+      
+    if not os.path.isdir(outputdirectoryprefix):
+      os.makedirs(outputdirectoryprefix)
+    elif os.path.isdir(outputdirectoryprefix):
+	  os.removedirs(outputdirectoryprefix)
+	  os.makedirs(outputdirectoryprefix)
     inventoryOutputFileStr = outputdirectoryprefix + '/magicdeckboxinventory.csv'
     decklistOutputFileStr = outputdirectoryprefix + '/magiccollection.dec'
-
-
-  if args.collection == '':
-    if os.path.isfile(home + '/Dropbox/MagicAssistantWorkspace/magiccards/Collections/main.xml'):
-      collectionFileStr = home + '/Dropbox/MagicAssistantWorkspace/magiccards/Collections/main.xml'
-    elif os.path.isfile(home + '/Ubuntu One/MagicAssistantWorkspace/magiccards/Collections/main.xml'):
-      collectionFileStr = home + '/Ubuntu One/MagicAssistantWorkspace/magiccards/Collections/main.xml'
+    
+  if args.singlecollectiononly:
+    if args.inventoryoutputfile != '' or args.inventoryoutputfile != '':
+      if (args.inventoryoutputfile != '' and args.inventoryoutputfile) or (args.inventoryoutputfile == '' and args.inventoryoutputfile != ''):
+        print('--inventoryoutputfile & --decklistoutputfile must both be specified or left to default')
+        from sys import exit
+        exit()
+      else:
+        outputdirectoryprefix = ''
+        inventoryOutputFileStr = args.inventoryoutputfile
+        decklistOutputFileStr = args.decklistoutputfile
+    if args.collection == '':
+      if os.path.isfile(home + '/Dropbox/MagicAssistantWorkspace/magiccards/Collections/main.xml'):
+        collectionFileStr = home + '/Dropbox/MagicAssistantWorkspace/magiccards/Collections/main.xml'
+      elif os.path.isfile(home + '/Ubuntu One/MagicAssistantWorkspace/magiccards/Collections/main.xml'):
+        collectionFileStr = home + '/Ubuntu One/MagicAssistantWorkspace/magiccards/Collections/main.xml'
+      else:
+        print('No suitable collection found!')
+        from sys import exit
+        exit()
     else:
-      print('No suitable collection found!')
+      collectionFileStr = args.collection
+      createfiles(collectionFileStr, inventoryOutputFileStr,decklistOutputFileStr)
+  else:
+    if os.path.exists(home + '/Dropbox/MagicAssistantWorkspace/magiccards/Collections/'):
+      collectionFolderStr = home + '/Dropbox/MagicAssistantWorkspace/magiccards/Collections/'
+    elif os.path.exists(home + '/Ubuntu One/MagicAssistantWorkspace/magiccards/Collections/'):
+      collectionFolderStr = home + '/Ubuntu One/MagicAssistantWorkspace/magiccards/Collections/'
+    else:
+      print('Please specify a valid folder.')
       from sys import exit
       exit()
-  else:
-    collectionFileStr = args.collection
-  
+    
+    import fnmatch
+    
+    listoffiles = []
+    for root, dirnames, filenames in os.walk(collectionFolderStr):
+      for filename in fnmatch.filter(filenames, '*.xml'):
+        listoffiles.append(os.path.join(root, filename))
+    for collectionfile in listoffiles:
+      cleanfilename = os.path.splitext(os.path.basename(collectionfile))[0]
+      inventoryOutputDynamicStr = outputdirectoryprefix + '/' + cleanfilename + ".csv"
+      decklistOutputDynamicStr = outputdirectoryprefix + '/' + cleanfilename + ".dec"
+      createfiles(collectionfile,inventoryOutputDynamicStr,decklistOutputDynamicStr)
+      
+def createfiles(collectionFileStr,inventoryOutputFileStr,decklistOutputFileStr):
   # Get the automatically converted CSV first (the staging file)
   generatedCSV = xml2csv(collectionFileStr)
   
@@ -59,7 +91,6 @@ def main():
 
   # Create a decklist version as well
   createdecklist(generatedCSV,decklistOutputFileStr)
-
 
 def createdeckboxheader(inventoryfile):
   import os
@@ -129,8 +160,6 @@ def createdeckboxinv(stagedcsv,inventoryfile):
         else:
           rdr.next()
 
-
-
 def createdecklist(stagedcsv,decklistfile):
   import csv
   from time import strftime
@@ -175,10 +204,10 @@ def xml2csv(collection):
     if event == 'start' and elem.tag == 'mcp' and not started:
       started = True
   
-    if started and event == 'end' and elem.tag != 'mcp':	#child nodes of the specified record tag
-      tags.append(elem.tag) if tagged == False else True	# csv header (element tag names)
+    if started and event == 'end' and elem.tag != 'mcp':    #child nodes of the specified record tag
+      tags.append(elem.tag) if tagged == False else True    # csv header (element tag names)
       items.append( '' if elem.text == None or elem.text.strip() == '' else elem.text.replace('"', '\\\"') )
-  	
+      
     # end of traversing the record tag
     if event == 'end' and elem.tag == 'mcp' and len(items) > 0:
       # csv header (element tag names)
@@ -189,12 +218,11 @@ def xml2csv(collection):
       output_buffer.append('\"' + ('\"' + ', ' + '\"').join(items) + '\"')
       items = []
       n+=1
-  		
+          
       # flush buffer to disk
       if len(output_buffer) > '1000':
         output.write(  '\n'.join(output_buffer) + '\n' )
         output_buffer = []
-  
   
   output.write(  '\n'.join(output_buffer) + '\n' )
   output_buffer = []
