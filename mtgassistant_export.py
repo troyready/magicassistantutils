88797d567573dae6b6fc4b3a17337aacbe894bc6
Troy Ready <troy@troyready.com>
2014-02-04 23:19:46 -0800

    Update mtgassistant_export.py
    Revert player rewards filter; was messing up export

--- mtgassistant_export.py
+++ mtgassistant_export.py
@@ -222,7 +222,7 @@ def createdeckboxinv(stagedcsv,inventoryfile):
         # Write the line to the file
         # Certain cards cannot be imported now due a deckbox bug; have to skip them and import them manually
         # http://deckbox.org/forum/viewtopic.php?pid=72629
-        if "loan to me" not in r[5] and r[4] != '0' and cardname != "Lim-Dûl's Vault" and cardname != "Chaotic Æther" and cardedition != "Magic Player Rewards":
+        if "loan to me" not in r[5] and r[4] != '0' and cardname != "Lim-Dûl's Vault" and cardname != "Chaotic Æther":
           wtr.writerow([r[4].replace("\"","").replace(" ","")] + [tradecount] + [cardname] + [foilstatus] + [r[3].replace("\"","").replace(" ","")] + [promostatus] + [r[3].replace("\"","").replace(" ","")] + [cardedition] + [condition] + ["English"])
         else:
           rdr.next()
